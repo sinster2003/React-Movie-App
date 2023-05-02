9604cec04d259e5f9ee142bb5295ce792cfa0da4
Sindhur <sindhurvshabaraya2318@gmail.com>
2023-05-02 17:50:44 +0530

    React Movie App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import MovieCard from "./MovieCard.jsx";
+import SearchIcon from "./search.svg";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [movies, setMovies] = useState([]);
+  const [searchMovie, setSearchMovie] = useState("");
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${process.env.REACT_APP_URL}&s=${title}`);
+    const data = await response.json();
+
+    setMovies(data.Search);
+  };
+
+  const handleEnter = (event) => {
+    if (event.key === "Enter") {
+      searchMovies(searchMovie);
+    }
+  };
+
+  useEffect(() => {
+    searchMovies("Batman");
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1 id="title">MovieLand</h1>
+      <div className="search">
+        <input
+          placeholder="Search for movies"
+          value={searchMovie}
+          onChange={(event) => {
+            setSearchMovie(event.target.value);
+          }}
+          onKeyDown={handleEnter}
+        />
+        <img
+          src={SearchIcon}
+          alt="Search Icon"
+          onClick={() => {
+            searchMovies(searchMovie);
+          }}
+        />
+      </div>
+      {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => {
+            return <MovieCard movie={movie} key={movie.imdbID} />;
+          })}
+        </div>
+      ) : (
+        <div>
+          <h1 id="notfound">No Movies Found</h1>
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default App;
